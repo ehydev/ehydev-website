68364dae105df5cfe5ed879df0ae8b5ce3638beb
ehydev <ehydevpro@gmail.com>
2025-07-24 18:31:33 +0200

    change user git and test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-900">
-        <h1 className="text-4xl font-bold">Bienvenida a EhyDev 🚀</h1>
+        <h1 className="text-4xl font-bold">Bienvenidxs a EhyDev 🚀</h1>
       </div>
     </>
   );
